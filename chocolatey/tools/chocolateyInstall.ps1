a8971a3642ea5229f4ce1231e1eff2454d96feaa
Rob Thijssen <rthijssen@gmail.com>
2015-10-05 15:06:07 +0100

    chocolatey package for v 1.11.0

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'mozilla-build'
 $fileType = 'exe'
-$url = 'http://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaBuildSetup-1.10.0.exe'
+$url = 'http://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaBuildSetup-1.11.0.exe'
 $silentArgs = '/S'
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
